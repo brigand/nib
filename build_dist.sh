1bdfb98ea96c8e883e4b84457a81d66e98c52056
Frankie Bagnardi <f.bagnardi@gmail.com>
2014-02-03 02:07:30 -0800

    dist scripts

---
+++ build_dist.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Ensure there aren't any unsaved changes here
+set -e
+git diff --exit-code
+
+# Build everything
+grunt dist
+
+# Move the dist somewhere safe
+TEMP_DIR=`sh tasks/get_temp_dir.sh`
+
+mkdir $TEMP_DIR || false
+mv dist/ $TEMP_DIR/
+
+# Save our node_modules and bower_components directories
+mv node_modules $TEMP_DIR/
+mv bower_components $TEMP_DIR/
+
+# Clean the working directory
+git checkout .
+
+# Bring back the dist files
+git checkout gh-pages
+mv $TEMP_DIR/dist/* ./
+
+# Display the changes, and then let the user commit, etc.
+git status
+
---
+++ tasks/get_binaries_back.sh
@@ -0,0 +1,6 @@
+TEMP_DIR=`sh tasks/get_temp_dir.sh`
+
+mv $TEMP_DIR/node_modules ./
+mv $TEMP_DIR/bower_components ./
+
+
